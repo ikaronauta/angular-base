9b0a6057a67bc1ea4e96d7439b4a9432cde155fa
ikaronauta <ortizgalindoj@gmail.com>
2024-08-04 18:40:34 -0500

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitilazedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
+  changeHero( newName: string ):void {
+    this.name = newName;
+  }
+
+  changeAge( newAge:number ):void {
+    this.age = newAge;
+  }
+
+  resetForm():void {
+    this.name = 'ironmane';
+    this.age = 45;
+  }
+}
